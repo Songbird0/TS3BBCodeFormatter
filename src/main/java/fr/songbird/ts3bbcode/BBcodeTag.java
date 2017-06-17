984918c912a5f965d0a0804965e2950a68ef1540
Anthony Defranceschi <chaacygg@gmail.com>
2017-06-17 23:29:31 +0200

    wrap method declaration.

--- src/main/java/fr/songbird/ts3bbcode/BBcodeTag.java
+++ src/main/java/fr/songbird/ts3bbcode/BBcodeTag.java
@@ -6,4 +6,11 @@ package fr.songbird.ts3bbcode;
  * @since 17/06/17
  */
 public abstract class BBcodeTag {
+    /**
+     * Wraps {@code stringToWrap} with {@code beginTag}, adds tag parameters (if any)
+     * then puts {@code endTag} at the end of sequence.
+     * @param stringToWrap The string that will be wrapped by bbcode tags.
+     * @return The wrapped sequence.
+     */
+    public abstract String wrap(final String stringToWrap);
 }
