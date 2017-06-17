984b9366b863a6f51ee6b270a485221e387523f3
Anthony Defranceschi <chaacygg@gmail.com>
2017-06-17 23:55:21 +0200

    correction

--- src/main/java/fr/songbird/ts3bbcode/Color.java
+++ src/main/java/fr/songbird/ts3bbcode/Color.java
@@ -2,7 +2,7 @@ package fr.songbird.ts3bbcode;
 
 /**
  * Built-in colors.
- * <p>If you want to use these constants, {@code toString()} method call isn't needed.</p>
+ * <p>If you want to use these constants, {@code toString()} method call is needed.</p>
  * @author anthony
  * @since 17/06/17
  */
