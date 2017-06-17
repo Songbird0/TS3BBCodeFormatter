78ad1beef2b70227361f2681f689553c606a6e03
Anthony Defranceschi <chaacygg@gmail.com>
2017-06-17 23:34:50 +0200

    BBcodeTag javadoc updated

--- src/main/java/fr/songbird/ts3bbcode/BBcodeTag.java
+++ src/main/java/fr/songbird/ts3bbcode/BBcodeTag.java
@@ -1,11 +1,24 @@
 package fr.songbird.ts3bbcode;
 
 /**
+ * Represents a bbcode tag.
+ * <p>
+ * All tags must extend BBcodeTag to inherit wrap() method.<br>
+ *
+ * BBcodeTag properties:
+ * </p>
+ * <ol>
+ *   <li>{@code beginTag}, e.g. {@code [b]}, {@code [url]};</li>
+ *   <li>{@code endTag}, e.g. {@code [b]}, {@code [url]};</li>
+ *   <li>{@code tagParameters}, e.g. {@code [url=duckduckgo.com]...[/url]} (duckduckgo.com is a parameter).</li>
+ * </ol>
+ * All of them can be empty.
  * @author anthony
  * @version 0.1.0
  * @since 17/06/17
  */
 public abstract class BBcodeTag {
+
     /**
      * Wraps {@code stringToWrap} with {@code beginTag}, adds tag parameters (if any)
      * then puts {@code endTag} at the end of sequence.
