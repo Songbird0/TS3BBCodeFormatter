a545fabf0456ceb7e74ce16c2fa1e3a9ad5f6b99
Anthony Defranceschi <chaacygg@gmail.com>
2017-06-17 22:56:48 +0200

    first unit test for Color enum

---
+++ src/test/java/fr/songbird/ts3bbcode/ColorTest.java
@@ -0,0 +1,26 @@
+package fr.songbird.ts3bbcode;
+
+import org.junit.Test;
+
+/**
+ * @author anthony
+ * @since 17/06/17
+ */
+public class ColorTest {
+
+    @Test
+    public void enumeration()
+    {
+        final Color red = Color.RED;
+        final Color green = Color.GREEN;
+        final Color blue = Color.BLUE;
+        final Color yellow = Color.YELLOW;
+        final Color orange = Color.ORANGE;
+        final Color grey = Color.GREY;
+        final Color purple = Color.PURPLE;
+        final Color brown = Color.BROWN;
+        final Color beige = Color.BEIGE;
+        final Color cyan = Color.CYAN;
+        final Color navy = Color.NAVY;
+    }
+}
