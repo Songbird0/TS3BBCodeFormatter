cfe2bc0e01ced697143135eef1ec345b9988f2fa
Anthony Defranceschi <chaacygg@gmail.com>
2017-06-17 23:07:48 +0200

    add colorRepresentation unit test

--- src/test/java/fr/songbird/ts3bbcode/ColorTest.java
+++ src/test/java/fr/songbird/ts3bbcode/ColorTest.java
@@ -2,6 +2,10 @@ package fr.songbird.ts3bbcode;
 
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 /**
  * @author anthony
  * @since 17/06/17
@@ -22,5 +26,32 @@ public class ColorTest {
         final Color beige = Color.BEIGE;
         final Color cyan = Color.CYAN;
         final Color navy = Color.NAVY;
+    }
+
+    @Test
+    public void colorRepresentation()
+    {
+        final Color red = Color.RED;
+        final Color green = Color.GREEN;
+        final Color blue = Color.BLUE;
+        final Color yellow = Color.YELLOW;
+        final Color orange = Color.ORANGE;
+        final Color grey = Color.GREY;
+        final Color purple = Color.PURPLE;
+        final Color brown = Color.BROWN;
+        final Color beige = Color.BEIGE;
+        final Color cyan = Color.CYAN;
+        final Color navy = Color.NAVY;
+        assertThat(red.toString(), is(equalTo("RED")));
+        assertThat(green.toString(), is(equalTo("GREEN")));
+        assertThat(blue.toString(), is(equalTo("BLUE")));
+        assertThat(yellow.toString(), is(equalTo("YELLOW")));
+        assertThat(orange.toString(), is(equalTo("ORANGE")));
+        assertThat(grey.toString(), is(equalTo("GREY")));
+        assertThat(purple.toString(), is(equalTo("PURPLE")));
+        assertThat(brown.toString(), is(equalTo("BROWN")));
+        assertThat(beige.toString(), is(equalTo("BEIGE")));
+        assertThat(cyan.toString(), is(equalTo("CYAN")));
+        assertThat(navy.toString(), is(equalTo("NAVY")));
     }
 }
