0e3f68c57457f8b8941c45d8b57a76f8b8a06c51
Anthony Defranceschi <chaacygg@gmail.com>
2017-06-18 01:09:04 +0200

    It's ok, all unit tests work.

--- src/test/java/fr/songbird/ts3bbcode/BBcodeTagTest.java
+++ src/test/java/fr/songbird/ts3bbcode/BBcodeTagTest.java
@@ -1,8 +1,21 @@
 package fr.songbird.ts3bbcode;
 
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 /**
  * @author anthony
  * @since 18/06/17
  */
 public class BBcodeTagTest {
+
+    @Test
+    public void addTagParameterTest()
+    {
+        final String result = new BBcodeColorTag(Color.BLUE).addTagParameter();
+        assertThat(result, is(equalTo("[color=BLUE]")));
+    }
 }
