e44f8c366f471442e6a14cff56ccef1458650952
Anthony Defranceschi <chaacygg@gmail.com>
2017-06-17 23:20:16 +0200

    Color enum declaration.

---
+++ src/main/java/fr/songbird/ts3bbcode/Color.java
@@ -0,0 +1,11 @@
+package fr.songbird.ts3bbcode;
+
+/**
+ * Built-in colors.
+ * <p>If you want to use these constants, {@code toString()} method call isn't needed.</p>
+ * @author anthony
+ * @since 17/06/17
+ */
+public enum Color {
+    GREEN, BLUE, YELLOW, ORANGE, GREY, PURPLE, BROWN, BEIGE, CYAN, NAVY, RED
+}
