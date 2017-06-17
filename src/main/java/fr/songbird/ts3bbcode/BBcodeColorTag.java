6ae23f156a844a2c90abddfbd20911c3eaf2db03
Anthony Defranceschi <chaacygg@gmail.com>
2017-06-17 22:45:33 +0200

    BBcodeColorTag class declared
    The super constructor will be modified later.

---
+++ src/main/java/fr/songbird/ts3bbcode/BBcodeColorTag.java
@@ -0,0 +1,12 @@
+package fr.songbird.ts3bbcode;
+
+/**
+ * @author anthony
+ * @version 0.1.0
+ * @since 17/06/17
+ */
+public class BBcodeColorTag extends BBcodeTag {
+    public BBcodeColorTag(final int rgbModel) {
+        super();
+    }
+}
