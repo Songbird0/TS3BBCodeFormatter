3fb515fa2615079fedd0bb95e9aa01b5488b1f60
Anthony Defranceschi <chaacygg@gmail.com>
2017-06-17 23:42:36 +0200

    javadoc updated

--- src/main/java/fr/songbird/ts3bbcode/BBcodeTag.java
+++ src/main/java/fr/songbird/ts3bbcode/BBcodeTag.java
@@ -1,5 +1,7 @@
 package fr.songbird.ts3bbcode;
 
+import com.sun.istack.internal.NotNull;
+
 /**
  * Represents a bbcode tag.
  * <p>
@@ -9,7 +11,7 @@ package fr.songbird.ts3bbcode;
  * </p>
  * <ol>
  *   <li>{@code beginTag}, e.g. {@code [b]}, {@code [url]};</li>
- *   <li>{@code endTag}, e.g. {@code [b]}, {@code [url]};</li>
+ *   <li>{@code endTag}, e.g. {@code [/b]}, {@code [/url]};</li>
  *   <li>{@code tagParameter}, e.g. {@code [url=duckduckgo.com]...[/url]} (duckduckgo.com is a parameter).</li>
  * </ol>
  * All of them can be empty.
@@ -19,14 +21,28 @@ package fr.songbird.ts3bbcode;
  */
 public abstract class BBcodeTag {
 
+    /**
+     * Tag pushed at the beginning (e.g. {@code [b]}, {@code [url]}).
+     */
+    @NotNull
     private final String beginTag;
+    /**
+     * Tag pushed at the end (e.g. {@code [/b]}, {@code [/url]}).
+     */
+    @NotNull
     private final String endTag;
+    /**
+     * The tag parameter (if any) pushed into tag (e.g. {@code [url=duckduckgo.com]...[/url]} (duckduckgo.com is a parameter)).
+     */
+    @NotNull
     private final String tagParameter;
+
     /**
      * Wraps {@code stringToWrap} with {@code beginTag}, adds tag parameters (if any)
      * then puts {@code endTag} at the end of sequence.
      * @param stringToWrap The string that will be wrapped by bbcode tags.
      * @return The wrapped sequence.
      */
+    @NotNull
     public abstract String wrap(final String stringToWrap);
 }
