0394784aca17855039aa36f60bf01dba333fcb10
Anthony Defranceschi <chaacygg@gmail.com>
2017-06-24 22:08:43 +0200

    splitted unit tests

--- src/test/java/fr/songbird/ts3bbcode/ColorTest.java
+++ src/test/java/fr/songbird/ts3bbcode/ColorTest.java
@@ -13,45 +13,69 @@ import static org.hamcrest.core.IsEqual.equalTo;
 public class ColorTest {
 
     @Test
-    public void enumeration()
+    public void redColorRepresentation()
     {
         final Color red = Color.RED;
-        final Color green = Color.GREEN;
-        final Color blue = Color.BLUE;
-        final Color yellow = Color.YELLOW;
-        final Color orange = Color.ORANGE;
-        final Color grey = Color.GREY;
-        final Color purple = Color.PURPLE;
-        final Color brown = Color.BROWN;
-        final Color beige = Color.BEIGE;
-        final Color cyan = Color.CYAN;
-        final Color navy = Color.NAVY;
-    }
-
-    @Test
-    public void colorRepresentation()
-    {
-        final Color red = Color.RED;
-        final Color green = Color.GREEN;
-        final Color blue = Color.BLUE;
-        final Color yellow = Color.YELLOW;
-        final Color orange = Color.ORANGE;
-        final Color grey = Color.GREY;
-        final Color purple = Color.PURPLE;
-        final Color brown = Color.BROWN;
-        final Color beige = Color.BEIGE;
-        final Color cyan = Color.CYAN;
-        final Color navy = Color.NAVY;
         assertThat(red.toString(), is(equalTo("RED")));
+    }
+    @Test
+    public void greenColorRepresentation()
+    {
+        final Color green = Color.GREEN;
         assertThat(green.toString(), is(equalTo("GREEN")));
+    }
+    @Test
+    public void blueColorRepresentation()
+    {
+        final Color blue = Color.BLUE;
         assertThat(blue.toString(), is(equalTo("BLUE")));
+    }
+    @Test
+    public void yellowColorRepresentation()
+    {
+        final Color yellow = Color.YELLOW;
         assertThat(yellow.toString(), is(equalTo("YELLOW")));
+    }
+    @Test
+    public void orangeColorRepresentation()
+    {
+        final Color orange = Color.ORANGE;
         assertThat(orange.toString(), is(equalTo("ORANGE")));
+    }
+    @Test
+    public void greyColorRepresentation()
+    {
+        final Color grey = Color.GREY;
         assertThat(grey.toString(), is(equalTo("GREY")));
+    }
+    @Test
+    public void purpleColorRepresentation()
+    {
+        final Color purple = Color.PURPLE;
         assertThat(purple.toString(), is(equalTo("PURPLE")));
+    }
+    @Test
+    public void brownColorRepresentation()
+    {
+        final Color brown = Color.BROWN;
         assertThat(brown.toString(), is(equalTo("BROWN")));
+    }
+    @Test
+    public void beigeColorRepresentation()
+    {
+        final Color beige = Color.BEIGE;
         assertThat(beige.toString(), is(equalTo("BEIGE")));
+    }
+    @Test
+    public void cyanColorRepresentation()
+    {
+        final Color cyan = Color.CYAN;
         assertThat(cyan.toString(), is(equalTo("CYAN")));
+    }
+    @Test
+    public void navyColorRepresentation()
+    {
+        final Color navy = Color.NAVY;
         assertThat(navy.toString(), is(equalTo("NAVY")));
     }
 }
