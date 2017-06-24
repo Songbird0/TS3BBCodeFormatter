785e83e86f06fa026da129c5271b01931f468731
Anthony Defranceschi <chaacygg@gmail.com>
2017-06-25 01:26:07 +0200

    BBcodeUnderlineTag unit tests.

---
+++ src/main/java/fr/songbird/ts3bbcode/BBcodeUnderlineTag.java
@@ -0,0 +1,12 @@
+package fr.songbird.ts3bbcode;
+
+/**
+ * @author anthony
+ * @since 25/06/17
+ */
+public class BBcodeUnderlineTag extends BBcodeTag {
+
+    public BBcodeUnderlineTag() {
+        super("[u]", "[/u]", "");
+    }
+}
---
+++ src/test/java/fr/songbird/ts3bbcode/BBcodeUnderlineTagTest.java
@@ -0,0 +1,27 @@
+package fr.songbird.ts3bbcode;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+/**
+ * @author anthony
+ * @since 25/06/17
+ */
+public class BBcodeUnderlineTagTest {
+
+    @Test
+    public void constructorTest1()
+    {
+        final BBcodeTag underlineTag = new BBcodeUnderlineTag();
+    }
+
+    @Test
+    public void wrapMethodTest()
+    {
+        final BBcodeTag underlineTag = new BBcodeUnderlineTag();
+        final String textMessageContent = underlineTag.wrap("My underline text message!");
+        assertThat(textMessageContent, equalTo("[u]My underline text message![/u]"));
+    }
+}
