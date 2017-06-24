affee8a090589d021a13b9f31221ad973c881530
Anthony Defranceschi <chaacygg@gmail.com>
2017-06-25 01:09:32 +0200

    BBcodeBoldTag unit tests.

---
+++ src/test/java/fr/songbird/ts3bbcode/BBcodeBoldTagTest.java
@@ -0,0 +1,26 @@
+package fr.songbird.ts3bbcode;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+/**
+ * @author anthony
+ * @since 25/06/17
+ */
+public class BBcodeBoldTagTest {
+
+    @Test
+    public void constructorTest1()
+    {
+        final BBcodeTag boldTag = new BBcodeBoldTag();
+    }
+    @Test
+    public void wrapMethodTest()
+    {
+        final BBcodeTag boldTag = new BBcodeBoldTag();
+        final String textMessageContent = boldTag.wrap("My text message is bold!");
+        assertThat(textMessageContent, equalTo("[b]My text message is bold![/b]"));
+    }
+}
