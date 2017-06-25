21826e42a2dd91a2d1ddfc34f69b34c3ff22451f
Anthony Defranceschi <chaacygg@gmail.com>
2017-06-25 03:50:44 +0200

    first contructor has been implemented

--- src/main/java/fr/songbird/ts3bbcode/BBcodeFormatter.java
+++ src/main/java/fr/songbird/ts3bbcode/BBcodeFormatter.java
@@ -1,5 +1,7 @@
 package fr.songbird.ts3bbcode;
 
+import java.util.Objects;
+
 /**
  * Formats your string with compatible TeamSpeak bbcode tags.
  * @author anthony
@@ -7,13 +9,42 @@ package fr.songbird.ts3bbcode;
  */
 public class BBcodeFormatter {
 
+    private StringBuilder builder = null;
+
     /**
      * Initializes the formatter, appends a string and wraps it.
      * <p>{@code bbcodeTags} as in will be used to format your string.</p>
      * @param s Your string.
      * @param bbcodeTags BBcode tags with which you want to format your string.
+     * @exception NullPointerException If your string is null.
+     * @exception NullPointerException If your bbcode tags list is null.
+     * @exception IllegalArgumentException If your string is empty.
+     * @exception IllegalArgumentException If your bbcode tags list is empty.
      */
     public BBcodeFormatter(String s, BBcodeTag... bbcodeTags) {
+        Objects.requireNonNull(s, "The string cannot be null.\n" +
+                "Please use the BBcodeFormatter() constructor instead.");
+        Objects.requireNonNull(bbcodeTags, "Your bbcode tags list cannot be null.\n" +
+                "Please use the BBcodeFormatter(String) constructor instead.");
+        if(s.isEmpty())
+            throw new IllegalArgumentException("The string cannot be empty.\n" +
+                    "Please use the BBcodeFormatter() constructor instead.");
+        if(bbcodeTags.length == 0)
+            throw new IllegalArgumentException("Your bbcode tags list cannot be empty.\n" +
+                    "Please use the BBcodeFormatter(String) constructor instead.");
+        this.builder = new StringBuilder();
+        String content = null;
+        for(int accumulator = 0; accumulator < bbcodeTags.length; accumulator += 1)
+        {
+            if(accumulator == 0)
+            {
+                content = bbcodeTags[0].wrap(s);
+                continue;
+            }
+            final String lastContentVersion = content;
+            content = bbcodeTags[accumulator].wrap(lastContentVersion);
+        }
+        builder.append(content);
     }
 
     /**
@@ -71,12 +102,12 @@ public class BBcodeFormatter {
      * @return Your text message wrapped, formatted.
      */
     public String toBBcodeString() {
-        return null;
+        return builder.toString();
     }
 
     @Override
     public String toString()
     {
-        return null;
+        return builder.toString();
     }
 }
