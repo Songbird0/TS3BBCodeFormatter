7c91f2dadbe0a7346b36ad23a008655bbcaed810
Anthony Defranceschi <chaacygg@gmail.com>
2017-06-25 03:24:32 +0200

    BBcodeFormatter class declaration.

--- src/main/java/fr/songbird/ts3bbcode/BBcodeFormatter.java
+++ src/main/java/fr/songbird/ts3bbcode/BBcodeFormatter.java
@@ -1,9 +1,82 @@
 package fr.songbird.ts3bbcode;
 
 /**
+ * Formats your string with compatible TeamSpeak bbcode tags.
  * @author anthony
- * @version majeure.mineure.bug
  * @since 16/06/17
  */
 public class BBcodeFormatter {
+
+    /**
+     * Initializes the formatter, appends a string and wraps it.
+     * <p>{@code bbcodeTags} as in will be used to format your string.</p>
+     * @param s Your string.
+     * @param bbcodeTags BBcode tags with which you want to format your string.
+     */
+    public BBcodeFormatter(String s, BBcodeTag... bbcodeTags) {
+    }
+
+    /**
+     * Initializes the formatter and appends a string.
+     * @param s Your string.
+     */
+    public BBcodeFormatter(final String s) {
+
+    }
+
+    /**
+     * Initializes the formatter.
+     */
+    public BBcodeFormatter() {
+
+    }
+
+    /**
+     * Puts a carriage return in your string.
+     * @return Formatter instance.
+     */
+    public BBcodeFormatter appendCarriageReturn() {
+        return null;
+    }
+
+    /**
+     * Puts your string into buffer and formats it.
+     * @param s Your string.
+     * @param bbcodeTags Your bbcode tags list.
+     */
+    public BBcodeFormatter append(String s, BBcodeTag... bbcodeTags) {
+        return null;
+    }
+
+    /**
+     * Puts your string into buffer and formats it.
+     * @param s Your string.
+     * @param bbcodeTag Your bbcode tag.
+     * @return Formatter instance.
+     */
+    public BBcodeFormatter append(String s, BBcodeTag bbcodeTag) {
+        return null;
+    }
+
+    /**
+     * Puts your string into buffer, no style applied.
+     * @param s Your string.
+     * @return Formatter instance.
+     */
+    public BBcodeFormatter append(String s) {
+        return null;
+    }
+
+    /**
+     * @return Your text message wrapped, formatted.
+     */
+    public String toBBcodeString() {
+        return null;
+    }
+
+    @Override
+    public String toString()
+    {
+        return null;
+    }
 }
--- src/test/java/fr/songbird/ts3bbcode/BBcodeFormatterTest.java
+++ src/test/java/fr/songbird/ts3bbcode/BBcodeFormatterTest.java
@@ -1,9 +1,67 @@
 package fr.songbird.ts3bbcode;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 /**
  * @author anthony
- * @version majeure.mineure.bug
  * @since 16/06/17
  */
 public class BBcodeFormatterTest {
+    @Rule
+    public final ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void constructorTest1()
+    {
+        final BBcodeFormatter formatter = new BBcodeFormatter("Simple buffered string. No bbcode here.");
+    }
+    @Test
+    public void constructorTest2()
+    {
+        final BBcodeFormatter formatter =
+                new BBcodeFormatter("Stylish buffered bbcode string.",
+                        new BBcodeColorTag(Color.PURPLE),
+                        new BBcodeItalicTag(),
+                        new BBcodeBoldTag(),
+                        new BBcodeUnderlineTag());
+    }
+    @Test
+    public void textFormattingTest1()
+    {
+        final BBcodeFormatter formatter = new BBcodeFormatter("Simple buffered string. No bbcode here.");
+        formatter.appendCarriageReturn()
+                .append("red text message.", new BBcodeColorTag(Color.RED))
+                .appendCarriageReturn()
+                .append("bold green message.", new BBcodeColorTag(Color.GREEN), new BBcodeBoldTag());
+        final String textMessageContent = formatter.toString();
+        assertThat(textMessageContent, equalTo("Simple buffered string. No bbcode here.\n" +
+                "[color=RED]red text message.[/color]\n" +
+                "[color=GREEN][b]bold green message.[/b][/color]"));
+    }
+    @Test
+    public void textFormattingTest2()
+    {
+        final BBcodeFormatter formatter = new BBcodeFormatter()
+                .append("raw text message")
+                .append(".");
+        final String textMessageContent = formatter.toBBcodeString();
+        assertThat(textMessageContent, equalTo("raw text message."));
+    }
+    @Test
+    public void textFormattingTest3()
+    {
+        final BBcodeFormatter formatter =
+                new BBcodeFormatter("Stylish buffered bbcode string.",
+                        new BBcodeColorTag(Color.PURPLE),
+                        new BBcodeItalicTag(),
+                        new BBcodeBoldTag(),
+                        new BBcodeUnderlineTag());
+        final String textMessageContent = formatter.toBBcodeString();
+        assertThat(textMessageContent, equalTo("[color=PURPLE][i][b][u]Stylish buffered bbcode string.[/u][/b][/i][/color]"));
+    }
 }
